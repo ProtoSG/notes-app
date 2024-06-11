70042399cc95650bb5b03d6a857a1881a8fdf420
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-06-10 23:49:16 -0500

    mod notepreview

--- src/renderer/src/components/NotePreview.tsx
+++ src/renderer/src/components/NotePreview.tsx
@@ -17,12 +17,17 @@ export const NotePreview = ({
 }: NotePreviewProps) => {
 
   const date = formateDateFromMs(updated_at)
+
+  const categories = ['personal', 'work', 'school', 'other']
+
+  const categoriesLimit = categories.filter((_, index) => index < 3)
+
   return (
     <div
       className={cn('cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75',
         {
-          'bg-zinc-400/75': isActive,
-          'hover:bg-zinc-500/75': !isActive,
+          'bg-zinc-400/25': isActive,
+          'hover:bg-zinc-400/25': !isActive,
         },
         className
       )}
@@ -30,6 +35,22 @@ export const NotePreview = ({
     >
       <h3 className='mb-1 font-bold truncate'>{title}</h3>
       <span className="inline-block w-full mb-2 text-xs font-light text-left">{date}</span>
+      {/* <div className='flex gap-2'> */}
+      {/*   {categoriesLimit.map((category) => ( */}
+      {/*     <small */}
+      {/*       key={category} */}
+      {/*       className={cn('uppercase border-2 rounded-md px-2 py-1 text-[10px]', */}
+      {/*         { */}
+      {/*           'border-red-400 text-red-400': category === 'personal', */}
+      {/*           'border-blue-400 text-blue-400': category === 'work', */}
+      {/*         } */}
+      {/*       )} */}
+      {/*     > */}
+      {/*       {category} */}
+      {/*     </small> */}
+      {/*   )) */}
+      {/*   } */}
+      {/* </div> */}
     </div>
   )
 }
